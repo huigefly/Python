f2a16e2e43aaf20ddcf41f3466881f81b99d84e5
zhengfh <zhengfh@sinoregal.cn>
2019-10-08 11:09:50 +0800

    基础语法记录

---
+++ basicgrammar.py
@@ -0,0 +1,23 @@
+#变量
+print("---value")
+value=100
+print("value:", value)
+print("value:{0}".format(value))
+
+
+#字符串
+print("---buffer test")
+buffer="helloworld"
+buffer2="nihao shijei"
+buffer3=buffer + buffer2
+print("size:", buffer.title())
+print(buffer3)
+
+#数字
+print("---number")
+v1=100
+v2=99
+v3=v1+v2+200
+msg="shenme " + str(v3) + " again!"
+print("v3:", v3)
+print("msg:", msg)
