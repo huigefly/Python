e83ffb5b7303b98b095f581afd17f0cef1b0a83c
zhengfh <438570993@qq.com>
2019-11-03 18:17:37 +0800

    添加python web按钮跳转调用shell尝试

---
+++ webdir/test.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import os
+
+#from flask import render_template
+
+app = Flask (__name__)
+
+@app.route("/")
+def hello():    
+	return "helloworld" 
+
+@app.route("/btn")
+def test():
+	return app.send_static_file("index.html")
+
+@app.route("/btn2")
+def btn2():
+    rtn=os.popen('ls -l').read()
+    return rtn
+if __name__ == "__main__":
+    app.run()
