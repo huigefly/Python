39e5bfeed9fabb3695f7e103bfc466c1e1e33bc5
zhengfuhui <438570993@qq.com>
2020-09-02 11:50:04 +0800

    update

--- multiprocess/5_fork.py
+++ multiprocess/5_fork.py
@@ -1,14 +1,25 @@
+#!/usr/bin/python2.7
+
 import os
 import time
+from logging import getLogger
+logger = getLogger()
+g_value = 10
 
-pid = os.fork()
-if pid == 0:
-    while 1:
-        print "helloworld"
-        time.sleep(10)
-        os._exit(0)
+try:
+    pid = os.fork()
+    if pid == 0:
+        while 1:
+            logger.info("i am child")
+            g_value = g_value -1
+            print g_value
+            time.sleep(10)
+            os._exit(0)
 
-if pid > 0:
-    print "parent wait"
-    os.waitpid(pid, 0)
-    print "parent end"
+    if pid > 0:
+        logger.info("parent wait here")
+        os.waitpid(pid, 0)
+        logger.info("parent end")
+
+except OSError, e:
+    print ("system error fork")
--- multithread/1_thread.py
+++ multithread/1_thread.py
@@ -2,11 +2,14 @@
 
 from threading import Thread
 import time
+from collections import deque
 
 # class no herit thread
 class CountdownTask:
     def __init__(self):
         self._running = True
+        self.history_list_length = 5 
+        self.history_list = deque(maxlen=5)
 
     def terminate(self):
         self._running = False
@@ -15,10 +18,18 @@ class CountdownTask:
         while self._running and n > 0:
             print('T-minus', n)
             n -= 1
-            time.sleep(5)
+            self.history_list.append(0.00)
+            if len(self.history_list) >= 5:
+                history_length = list(set(self.history_list))
+                if len(history_length) == 1:
+                    print("it is true 1")
+            
+            print("it is false")
+            time.sleep(1)
+    
 
 c = CountdownTask()
 t = Thread(target=c.run, args=(10,))
 t.start()
-c.terminate() # Signal termination
+# c.terminate() # Signal termination
 t.join() # Wait for actual termination (if needed)
